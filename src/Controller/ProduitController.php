21ef261ccea1a9cac9091c7d913f82a65719dba6
NairoD34 <peiro.dorian@gmail.com>
2023-12-18 16:32:42 +0100

    de la d

--- src/Controller/ProduitController.php
+++ src/Controller/ProduitController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\ProduitRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -9,10 +10,13 @@ use Symfony\Component\Routing\Annotation\Route;
 class ProduitController extends AbstractController
 {
     #[Route('/produit', name: 'app_produit')]
-    public function index(): Response
+    public function index(ProduitRepository $produitRepo): Response
     {
+        $produits = $produitRepo->searchNew();
+        var_dump($produits);
         return $this->render('produit/index.html.twig', [
             'controller_name' => 'ProduitController',
+            'produits' => $produits
         ]);
     }
 }
