466dd39ab041fb50971b4c6803f9232556fd4525
NairoD34 <peiro.dorian@gmail.com>
2023-12-21 15:56:28 +0100

    debut commande

--- src/Controller/CommandeController.php
+++ src/Controller/CommandeController.php
@@ -2,17 +2,28 @@
 
 namespace App\Controller;
 
+use App\Entity\Livraison;
+use App\Repository\LivraisonRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Bundle\SecurityBundle\Security;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 class CommandeController extends AbstractController
 {
     #[Route('/commande', name: 'app_commande')]
-    public function index(): Response
+    public function index(Security $security, LivraisonRepository $livraisonRepo): Response
     {
+        if (!$security->isGranted('IS_AUTHENTICATED_FULLY')) {
+            return $this->redirectToRoute('app_index');
+        }
+
+        $livraison = $livraisonRepo->findAll();
+
+
         return $this->render('commande/index.html.twig', [
             'controller_name' => 'CommandeController',
+            'livraisons' => $livraison,
         ]);
     }
 }
