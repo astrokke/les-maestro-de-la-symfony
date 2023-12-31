8b7d4ecff4213ba8cdc51d8499346c67201462c4
GaelBuenoBarthe <gaelbueno@aol.com>
2023-12-31 17:54:54 +0100

    modif template front

--- migrations/Version20231231162106.php
+++ migrations/Version20231231162106.php
@@ -10,7 +10,7 @@ use Doctrine\Migrations\AbstractMigration;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-final class Version20231231132959 extends AbstractMigration
+final class Version20231231162106 extends AbstractMigration
 {
     public function getDescription(): string
     {
@@ -20,7 +20,7 @@ final class Version20231231132959 extends AbstractMigration
     public function up(Schema $schema): void
     {
         // this up() migration is auto-generated, please modify it to your needs
-        $this->addSql('ALTER TABLE adresse ADD is_active TINYINT(1) NOT NULL');
+        $this->addSql('ALTER TABLE adresse ADD is_active TINYINT(1) DEFAULT NULL');
         $this->addSql('ALTER TABLE code_postal CHANGE libelle libelle VARCHAR(10) DEFAULT NULL');
     }
 
